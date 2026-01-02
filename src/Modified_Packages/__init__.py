02d972b2bd915806813edf0287dead4fce3db626
SleepyPandas <tommyrobotics1@gmail.com>
2026-01-01 23:20:20 -0500

    import * for testing
    Signed-off-by: SleepyPandas <tommyrobotics1@gmail.com>

--- src/Modified_Packages/__init__.py
+++ src/Modified_Packages/__init__.py
@@ -7,5 +7,6 @@ from .pdf2image import convert_from_path as convert_from_path
 from .pdf2image import pdfinfo_from_bytes as pdfinfo_from_bytes
 from .pdf2image import pdfinfo_from_path as pdfinfo_from_path
 
+from .pdf2image import *
 
 
